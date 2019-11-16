a20e0d845c116f90ed5a23dcb7851377a2440548
kazuw0401 <kazumasaw0401@gmail.com>
2019-11-16 19:16:24 +0900

    青本chapter2終了

--- app/routes/web.php
+++ app/routes/web.php
@@ -15,30 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 {font-size:100pt; text-align:right; color:#eee;
-    margin:-40px 0px -50px 0px }
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>This is sample page.</p>
-    <p>これはサンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
-
-Route::get('/hello', function () use ($html) {
-    return $html;
-});
-
 Route::get('/check', 'CodeCreateController@contact');
 Route::get('/contact', 'CodeCreateController@check');
+
+// 認証
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Laravel青本のコード
+Route::get('/hello', 'HelloController@index');
