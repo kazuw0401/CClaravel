c061786f40867d0d1cbeeba5a28dcbe771233c32
kazuw0401 <kazumasaw0401@gmail.com>
2019-11-18 23:51:13 +0900

    ビューコンボーザを用意

---
+++ app/app/Providers/HelloServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class HelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view::composer(
+            'hello.index', function ($view) {
+            $view->with('view_message', 'composer message!');
+        });
+    }
+}
