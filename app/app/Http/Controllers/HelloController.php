3f21fe940e31a0673380951635beeb416dc86059
kazuw0401 <kazumasaw0401@gmail.com>
2019-11-21 18:46:11 +0900

    青本4章終了

--- app/app/Http/Controllers/HelloController.php
+++ app/app/Http/Controllers/HelloController.php
@@ -12,45 +12,24 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        // $validator = Validator::make($request->query(), [
-        //     'id' => 'required',
-        //     'pass' => 'required',
-        // ]);
-        // if($validator->fails()) {
-        //     $msg = 'クエリに問題があります。';
-        // }else {
-        //     $msg = 'ID/PASSを受け付けました。フォームを入力ください。';
-        // }
-        return view('hello.index', ['msg' => 'フォームを入力ください。',]);
+        if($request->hasCookie('msg'))
+        {
+            $msg = 'Cookie: ' . $request->cookie('msg');
+        }else {
+            $msg = '※クッキーはありません。';
+        }
+        return view('hello.index', ['msg' => $msg]);
     } 
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        // $rules = [
-        //     'name' => 'required',
-        //     'mail' => 'email',
-        //     'age' => 'numeric',
-        // ];
-        // $messages = [
-        //     'name.required' => '名前は必ず入力してください。',
-        //     'mail.email' => 'メールアドレスが必要です。',
-        //     'age.numeric' => '年齢を整数で記入下さい。',
-        //     'age.min' => '年齢はゼロ歳以上で入力ください。',
-        //     'age.max' => '年齢は150歳以下で入力ください。',
-        // ];
-        // $validator = Validator::make($request->all(), $rules, $messages);
-
-        // $validator->sometimes('age', 'min:0', function($input) {
-        //     return !is_int($input->age);
-        // });
-        // $validator->sometimes('age', 'max:150', function($input) {
-        //     return !is_int($input->age);
-        // });
-        // if($validator->fails()) {
-        //     return redirect('/hello')
-        //                 ->withErrors($validator)
-        //                 ->withInput();
-        // }
-            return view('hello.index', ['msg' => '正しく入力されました！']);
+        $validate_rule = [
+            'msg' => 'required',
+        ];
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = new Response(view('hello.index', ['msg' => '「' . $msg .'」をクッキーに保存しました。']));
+        $response->cookie('msg', $msg, 100);
+            return $response;
         }
 }
--- app/resources/views/hello/index.blade.php
+++ app/resources/views/hello/index.blade.php
@@ -15,39 +15,18 @@
     <table>
         <form action="/hello" method="post">
         {{ csrf_field() }}
-        @if ($errors->has('name'))
+        @if ($errors->has('msg'))
         <tr>
-            <th>EROOR</th>
-            <td>{{$errors->first('name')}}</td>
+            <th>ERROR</th>
+            <td>{{$errors->first('msg')}}</td>
         </tr>
         @endif
         <tr>
-            <th>name: </th>
-            <td><input type="text" name="name" value="{{old('name')}}"></td>
-        </tr>
-        @if ($errors->has('mail'))
-        <tr>
-            <th>EROOR</th>
-            <td>{{$errors->first('mail')}}</td>
-        </tr>
-        @endif
-        <tr>
-            <th>mail: </th>
-            <td><input type="text" name="mail" value="{{old('mail')}}"></td>
-        </tr>
-        @if ($errors->has('age'))
-        <tr>
-            <th>EROOR</th>
-            <td>{{$errors->first('age')}}</td>
-        @endif
-        </tr>
-        <tr>
-            <th>age: </th>
-            <td><input type="text" name="age" value="{{old('age')}}"></td>
-        </tr>
-        <tr>
-            <th></th>
-            <td><input type="submit" value="send"></td>
+            <th>Message: </th>
+            <td><input type="text" name="msg" value="{{old('msg')}}"></td>
+            <tr>
+                <th><input type="submit" value="send"></th>
+            </tr>
         </tr>
         </form>
     </table>
