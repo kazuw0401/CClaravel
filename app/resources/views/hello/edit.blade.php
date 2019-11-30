7e8f16ccac16c221d8c10789a2b337eed8331755
kazuw0401 <kazumasaw0401@gmail.com>
2019-12-01 00:06:40 +0900

    データベース更新機能を実装

--- app/resources/views/hello/edit.blade.php
+++ app/resources/views/hello/edit.blade.php
@@ -8,25 +8,26 @@
 
 @section('content')
     <table>
-        <form action="/hello/edit" method="post"></form>
+        <form action="/hello/edit" method="post">
         @csrf
-        <input type="hidden" name="id" value="{{$form->id}}">
-        <tr>
-            <th>name: </th>
-            <td><input type="text" name="name" value="{{$form->name}}"></td>
-        </tr>
-        <tr>
-            <th>mail: </th>
-            <td><input type="text" name="mail" value="{{$form->mail}}"></td>
-        </tr>
-        <tr>
-            <th>age: </th>
-            <td><input type="text" name="age" value="{{$form->age}}"></td>
-        </tr>
-        <tr>
-            <th></th>
-            <td><input type="submit" value="send"></td>
-        </tr>
+            <input type="hidden" name="id" value="{{$form->id}}">
+            <tr>
+                <th>name: </th>
+                <td><input type="text" name="name" value="{{$form->name}}"></td>
+            </tr>
+            <tr>
+                <th>mail: </th>
+                <td><input type="text" name="mail" value="{{$form->mail}}"></td>
+            </tr>
+            <tr>
+                <th>age: </th>
+                <td><input type="text" name="age" value="{{$form->age}}"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="send"></td>
+            </tr>
+        </form>
     </table>
 @endsection
 @section('footer')
