de6deed770ce343ce5e70ca34e13ef5fb167749b
kazuw0401 <kazumasaw0401@gmail.com>
2019-12-02 23:29:53 +0900

    クエリビルダで指定したIDを取得する機能を実装

---
+++ app/resources/views/hello/show.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Show')
+@section('menubar')
+    @parent
+    詳細ページ
+@endsection
+
+@section('content')
+<table>
+    <tr>
+        <th>id: </th>
+        <td>{{$item->id}}</td>
+    </tr>
+    <tr>
+        <th>name: </th>
+        <td>{{$item->name}}</td>
+    </tr>
+    <tr>
+        <th>mail: </th>
+        <td>{{$item->mail}}</td>
+    </tr>
+    <tr>
+        <th>age: </th>
+        <td>{{$item->age}}</td>
+    </tr>
+</table>
+@endsection
+@section('footer')
+    &copy; 2019 kazuw0401
+@endsection
