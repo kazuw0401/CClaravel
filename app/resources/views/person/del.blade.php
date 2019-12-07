79adca1043c1cfbe35b3bb5952773fa0e068475e
kazuw0401 <kazumasaw0401@gmail.com>
2019-12-07 23:24:17 +0900

    青本6-3終了。モデルの削除機能を実装

---
+++ app/resources/views/person/del.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Person.Delete')
+@section('menubar')
+    @parent
+    削除ページ
+@endsection
+
+@section('content')
+    <table>
+        <form action="/person/del" method="post">
+            @csrf
+            <input type="hidden" name="id" value="{{$form->id}}">
+            <tr>
+                <th>name: </th>
+            </tr>
+            <tr>
+                <td><input type="text" name="name" value="{{$form->name}}"></td>
+            </tr>
+            <tr>
+                <th>mail: </th>
+            </tr>
+            <tr>
+                <td><input type="text" name="mail" value="{{$form->mail}}"></td>
+            </tr>
+            <tr>
+                <th>age: </th>
+            </tr>
+            <tr>
+                <td><input type="number" name="age" value="{{$form->age}}"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="send"></td>
+            </tr>
+        </form>
+    </table>
+@endsection
+@section('footer')
+    &copy; 2019 kazuw0401
+@endsection
